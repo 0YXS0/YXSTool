3ba5f04de3b45e0f3c6d8b1f2a1dc819b2c8595d
YXS <2436172910@qq.com>
2026-01-02 00:30:53 +0800

    添加FocTool的基本页面

---
+++ YXSFocTool/FocToolPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace YXSFocTool;
+
+/// <summary>
+/// FocToolPage.xaml 的交互逻辑
+/// </summary>
+public partial class FocToolPage : UserControl
+{
+    public FocToolPage( )
+    {
+        InitializeComponent( );
+    }
+}
