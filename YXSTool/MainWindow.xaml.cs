3c5c5034c2a8749faa2ac2fd799e9f5f49858a0b
YXS <2436172910@qq.com>
2026-01-01 16:05:54 +0800

    添加项目文件。

---
+++ YXSTool/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace YXSTool;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow( )
+    {
+        InitializeComponent( );
+    }
+}
