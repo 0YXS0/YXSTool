ef4c6170d9ff4f01bd9e2a07068fdfc8fcc53f4d
YXS <2436172910@qq.com>
2026-01-01 23:28:14 +0800

    完整主界面的无边框设计

--- YXSTool/MainWindow.xaml.cs
+++ YXSTool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace YXSTool;
 
@@ -10,5 +11,23 @@ public partial class MainWindow : Window
     public MainWindow( )
     {
         InitializeComponent( );
+        this.MouseMove += (s, e) => { if(e.LeftButton == MouseButtonState.Pressed) this.DragMove( ); };
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close( );  // 关闭窗口
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        if(this.WindowState != WindowState.Minimized)
+        {
+            this.WindowState = WindowState.Minimized; // 最小化窗口
+        }
+        else
+        {
+            this.WindowState = WindowState.Normal;    // 恢复窗口
+        }
     }
 }
