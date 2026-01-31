08e4f44b69f16e084974cc372785a1f0de23c4f5
YXS <2436172910@qq.com>
2026-02-01 00:28:05 +0800

    添加无边框窗口与圆角边框，并完善主界面功能

--- YXSTool/ViewModel/MainViewModel.cs
+++ YXSTool/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using MaterialDesignThemes.Wpf;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
+using System.Windows.Input;
 using YXSFocTool;
 
 namespace YXSTool.ViewModel;
@@ -38,6 +39,27 @@ public partial class MainViewModel : ObservableObject
         }
     }
 
+    [RelayCommand]
+    public static void MoveAndDoubleClick(MouseButtonEventArgs e)
+    {
+        var mainWindow = System.Windows.Application.Current.MainWindow;
+        if(e.LeftButton == MouseButtonState.Pressed && e.ClickCount == 1)
+        {
+            mainWindow.DragMove( );
+        }
+        else if(e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
+        {
+            if(mainWindow.WindowState != System.Windows.WindowState.Maximized)
+            {
+                mainWindow.WindowState = System.Windows.WindowState.Maximized; // 最大化窗口
+            }
+            else
+            {
+                mainWindow.WindowState = System.Windows.WindowState.Normal;    // 恢复窗口
+            }
+        }
+    }
+
     [ObservableProperty]
     private ObservableCollection<TabItemViewModel> tabItems = [
         new TabItemViewModel(TabItemInfos[0].Icon, TabItemInfos[0].Header, TabItemInfos[0].Factory()),
@@ -47,9 +69,18 @@ public partial class MainViewModel : ObservableObject
     private int selectedTabIndex = 0;
 
     [RelayCommand]
-    public void AddTabItem(string tabName)
+    public void AddTabItem(object? param)
     {
-        var tabInfo = TabItemInfos.FirstOrDefault(t => t.Header == tabName);
+        TabItemInfo? tabInfo = null;
+        if(TabItemInfos.Length == 1 && param is MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            tabInfo = TabItemInfos[0];
+        }
+        else if(param is string tabHeader)
+        {
+            tabInfo = TabItemInfos.FirstOrDefault(t => t.Header == tabHeader);
+        }
         if(tabInfo is not null)
         {
             TabItems.Add(new TabItemViewModel(
@@ -65,8 +96,11 @@ public partial class MainViewModel : ObservableObject
     [RelayCommand]
     public void RemoveTabItem(string header)
     {
-        var tabInfo = TabItems.FirstOrDefault(t => t.Header == header);
-        if(tabInfo is not null)
-            TabItems.Remove(tabInfo);
+        if(TabItems.Count > 1)
+        {
+            var tabInfo = TabItems.FirstOrDefault(t => t.Header == header);
+            if(tabInfo is not null)
+                TabItems.Remove(tabInfo);
+        }
     }
 }
