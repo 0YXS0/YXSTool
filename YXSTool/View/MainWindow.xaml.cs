4b01a6985e2c13442d987e5cef076de5ba3926bb
YXS <2436172910@qq.com>
2026-02-02 21:03:29 +0800

    Revert "添加无边框窗口与圆角边框，并完善主界面功能"
    This reverts commit 08e4f44b69f16e084974cc372785a1f0de23c4f5.

--- YXSTool/View/MainWindow.xaml.cs
+++ YXSTool/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using YXSTool.ViewModel;
 
 namespace YXSTool.View;
@@ -15,5 +16,36 @@ public partial class MainWindow : Window
         InitializeComponent( );
         this.DataContext = ViewModel;
         this.MouseDown += (_, _) => { Keyboard.ClearFocus( ); };
+    }
+
+    private void TabControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        var temp = VisualTreeHelper.GetParent(e.OriginalSource as DependencyObject);
+        temp = VisualTreeHelper.GetParent(temp);
+        if(temp == sender && e.ChangedButton == MouseButton.Left)
+        {
+            if(this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized; // 最大化窗口
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;    // 恢复窗口
+            }
+        }
+    }
+
+    private void TabControl_MouseMove(object sender, MouseEventArgs e)
+    {
+        try
+        {
+            var temp = VisualTreeHelper.GetParent(e.OriginalSource as DependencyObject);
+            temp = VisualTreeHelper.GetParent(temp);
+            if(temp == sender && e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove( );
+            }
+        }
+        catch { }
     }
 }
--- YXSTool/ViewModel/MainViewModel.cs
+++ YXSTool/ViewModel/MainViewModel.cs
@@ -3,7 +3,6 @@ using CommunityToolkit.Mvvm.Input;
 using MaterialDesignThemes.Wpf;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Input;
 using YXSFocTool;
 
 namespace YXSTool.ViewModel;
@@ -39,27 +38,6 @@ public partial class MainViewModel : ObservableObject
         }
     }
 
-    [RelayCommand]
-    public static void MoveAndDoubleClick(MouseButtonEventArgs e)
-    {
-        var mainWindow = System.Windows.Application.Current.MainWindow;
-        if(e.LeftButton == MouseButtonState.Pressed && e.ClickCount == 1)
-        {
-            mainWindow.DragMove( );
-        }
-        else if(e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
-        {
-            if(mainWindow.WindowState != System.Windows.WindowState.Maximized)
-            {
-                mainWindow.WindowState = System.Windows.WindowState.Maximized; // 最大化窗口
-            }
-            else
-            {
-                mainWindow.WindowState = System.Windows.WindowState.Normal;    // 恢复窗口
-            }
-        }
-    }
-
     [ObservableProperty]
     private ObservableCollection<TabItemViewModel> tabItems = [
         new TabItemViewModel(TabItemInfos[0].Icon, TabItemInfos[0].Header, TabItemInfos[0].Factory()),
@@ -69,18 +47,9 @@ public partial class MainViewModel : ObservableObject
     private int selectedTabIndex = 0;
 
     [RelayCommand]
-    public void AddTabItem(object? param)
+    public void AddTabItem(string tabName)
     {
-        TabItemInfo? tabInfo = null;
-        if(TabItemInfos.Length == 1 && param is MouseButtonEventArgs e)
-        {
-            e.Handled = true;
-            tabInfo = TabItemInfos[0];
-        }
-        else if(param is string tabHeader)
-        {
-            tabInfo = TabItemInfos.FirstOrDefault(t => t.Header == tabHeader);
-        }
+        var tabInfo = TabItemInfos.FirstOrDefault(t => t.Header == tabName);
         if(tabInfo is not null)
         {
             TabItems.Add(new TabItemViewModel(
@@ -96,11 +65,8 @@ public partial class MainViewModel : ObservableObject
     [RelayCommand]
     public void RemoveTabItem(string header)
     {
-        if(TabItems.Count > 1)
-        {
-            var tabInfo = TabItems.FirstOrDefault(t => t.Header == header);
-            if(tabInfo is not null)
-                TabItems.Remove(tabInfo);
-        }
+        var tabInfo = TabItems.FirstOrDefault(t => t.Header == header);
+        if(tabInfo is not null)
+            TabItems.Remove(tabInfo);
     }
 }
