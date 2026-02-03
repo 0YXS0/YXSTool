39dec589160f7dc9bb919c1b4fa17296ba33d57a
YXS <2436172910@qq.com>
2026-02-03 21:02:51 +0800

    完善主界面

--- YXSTool/ViewModel/MainViewModel.cs
+++ YXSTool/ViewModel/MainViewModel.cs
@@ -3,15 +3,17 @@ using CommunityToolkit.Mvvm.Input;
 using iNKORE.UI.WPF.Modern.Common.IconKeys;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
+using System.Windows.Input;
 using YXSFocTool;
 
 namespace YXSTool.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
-    public record TabItemInfo(FontIconData Icon, string Name, Func<ContentControl> Factory);
-    static private TabItemInfo[] TabItemInfos { get; } = [
-        new TabItemInfo(SegoeFluentIcons.Settings, "FocTool", ()=>new FocToolPage( )),
+    public record TabItemViewModel(FontIconData Icon, string Header, ContentControl Content);
+    public record TabItemInfo(FontIconData Icon, string Header, Func<ContentControl> Factory);
+    static public TabItemInfo[] TabItemInfos { get; } = [
+        new TabItemInfo(SegoeFluentIcons.Repair, "FocTool", ()=>new FocToolPage( )),
     ];
 
     [RelayCommand]
@@ -37,34 +39,68 @@ public partial class MainViewModel : ObservableObject
         }
     }
 
+    [RelayCommand]
+    public static void MoveAndDoubleClick(MouseButtonEventArgs e)
+    {
+        var mainWindow = System.Windows.Application.Current.MainWindow;
+        if(e.LeftButton == MouseButtonState.Pressed && e.ClickCount == 1)
+        {
+            mainWindow.DragMove( );
+        }
+        else if(e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
+        {
+            if(mainWindow.WindowState != System.Windows.WindowState.Maximized)
+            {
+                mainWindow.WindowState = System.Windows.WindowState.Maximized; // 最大化窗口
+            }
+            else
+            {
+                mainWindow.WindowState = System.Windows.WindowState.Normal;    // 恢复窗口
+            }
+        }
+    }
+
     [ObservableProperty]
-    private ObservableCollection<TabItem> tabItems = [
-        new TabItem( ) { Header = TabItemInfos[0].Name, Content = new FocToolPage( ), DataContext = TabItemInfos[0]}
+    private ObservableCollection<TabItemViewModel> tabItems = [
+        new TabItemViewModel(TabItemInfos[0].Icon, TabItemInfos[0].Header, TabItemInfos[0].Factory()),
     ];
 
     [ObservableProperty]
     private int selectedTabIndex = 0;
 
     [RelayCommand]
-    public void AddTabItem(string tabName)
+    public void AddTabItem(object? param)
     {
-        var tabInfo = TabItemInfos.FirstOrDefault(t => t.Name == tabName);
+        TabItemInfo? tabInfo = null;
+        if(TabItemInfos.Length == 1 && param is MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            tabInfo = TabItemInfos[0];
+        }
+        else if(param is string tabHeader)
+        {
+            tabInfo = TabItemInfos.FirstOrDefault(t => t.Header == tabHeader);
+        }
         if(tabInfo is not null)
         {
-            TabItems.Add(new TabItem( )
-            {
-                Header = tabInfo.Name,
-                Content = tabInfo.Factory( ),
-                DataContext = tabInfo
-            });
-            if(TabItems.Count == 1)
-                SelectedTabIndex = 0;
+            TabItems.Add(new TabItemViewModel(
+                tabInfo.Icon,
+                tabInfo.Header,
+                tabInfo.Factory( )
+            ));
+            if(TabItems.Count >= 1)
+                SelectedTabIndex = TabItems.Count - 1;
         }
     }
 
     [RelayCommand]
-    public void RemoveTabItem(TabItem tabItem)
+    public void RemoveTabItem(string header)
     {
-        TabItems.Remove(tabItem);
+        if(TabItems.Count > 1)
+        {
+            var tabInfo = TabItems.FirstOrDefault(t => t.Header == header);
+            if(tabInfo is not null)
+                TabItems.Remove(tabInfo);
+        }
     }
 }
